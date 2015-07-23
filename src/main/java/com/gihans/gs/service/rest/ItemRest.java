350edd007c66ac76ef7ce323f7a44a0c90fefc57
echamam <chaminda.amarasinghe@ericsson.com>
2015-07-23 23:30:46 +0100

    add item with categories

--- src/main/java/com/gihans/gs/service/rest/ItemRest.java
+++ src/main/java/com/gihans/gs/service/rest/ItemRest.java
@@ -5,7 +5,9 @@
  */
 package com.gihans.gs.service.rest;
 
+import com.gihans.gs.model.Category;
 import com.gihans.gs.model.Item;
+import com.gihans.gs.utils.StringUtils;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,7 +23,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
@@ -50,6 +51,14 @@ public class ItemRest {
             item.setStock(readInteger(input, "stock"));
             item.setDescription(readString(input, "description"));
             item.setStartFrom(readDate(input, "startFrom"));
+            item.setActive(Boolean.TRUE);
+            final String subCat = readString(input, "subCategory");
+            final String cat = readString(input, "category");
+            if (StringUtils.isEmpty(subCat)) {
+                item.setCategory(em.find(Category.class, Integer.valueOf(subCat)));
+            } else {
+                item.setCategory(em.find(Category.class, Integer.valueOf(cat)));
+            }
             em.persist(item);
             em.flush();
             item.setMainImage(saveImageAndGetPath(item.getId(), input, "mainImage"));
@@ -78,39 +87,14 @@ public class ItemRest {
 
     }
 
-    /**
-     * header sample { Content-Type=[image/png], Content-Disposition=[form-data;
-     * name="file"; filename="filename.extension"] }
-     *
-     */
-    //get uploaded filename, is there a easy way in RESTEasy?
-    private String getFileName(MultivaluedMap<String, String> header) {
-
-        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
-
-        for (String filename : contentDisposition) {
-            if ((filename.trim().startsWith("filename"))) {
-
-                String[] name = filename.split("=");
-
-                String finalFileName = name[1].trim().replaceAll("\"", "");
-                return finalFileName;
-            }
-        }
-        return "unknown";
-    }
-
     //save to somewhere
     private void writeFile(byte[] content, String filename) throws IOException {
 
-        File file = new File(filename);
-
+        final File file = new File(filename);
         if (!file.exists()) {
             file.createNewFile();
         }
-
-        FileOutputStream fop = new FileOutputStream(file);
-
+        final FileOutputStream fop = new FileOutputStream(file);
         fop.write(content);
         fop.flush();
         fop.close();
@@ -151,7 +135,7 @@ public class ItemRest {
 
     private Date readDate(final MultipartFormDataInput input, final String startFrom) {
         try {
-            return new SimpleDateFormat("MM/dd/yyyy").parse(input.getFormDataMap().get(startFrom).get(0).getBodyAsString());
+            return new SimpleDateFormat("yyyy-MM-dd").parse(input.getFormDataMap().get(startFrom).get(0).getBodyAsString());
         } catch (IOException | ParseException ex) {
             return new Date();
         }
---
+++ src/main/java/com/gihans/gs/utils/StringUtils.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gihans.gs.utils;
+
+/**
+ *
+ * @author echamam
+ */
+public class StringUtils {
+    
+    public static boolean isEmpty(final String s) {
+        return null != s && !s.isEmpty();
+    }
+    
+}
